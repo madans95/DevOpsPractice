d4f0e740e6cbd978f52d8a8538b1a0349aed49dd
S <madanks@deloitte.com>
2023-07-15 19:13:55 +0530

    Added Python assignment code and config file

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+#https://www.markdownguide.org/basic-syntax/
+#herovired_devops_classcodes
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def helloWorld():
     return "Hello World!!"
+
+@app.route("/aboutus", methods=['GET'])
+def aboutUS():
+    return "About Us"
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
---
+++ python_assignment/add.py
@@ -0,0 +1,3 @@
+a = int(input("Enter Num1: "))
+b = int(input("Enter Num2: "))
+print(a+b)
